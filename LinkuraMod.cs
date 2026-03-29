99c3b20590df29dc92dfba07768f1a04387cf09e
KCFindstr <792491320@qq.com>
2026-03-28 17:52:50 -0700

    Initial commit

---
+++ LinkuraMod.cs
@@ -0,0 +1,11 @@
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Modding;
+
+namespace RuriMegu;
+
+[ModInitializer("Initialize")]
+public static class LinkuraMod {
+  public static void Initialize() {
+    Log.Info("Link! Like! LoveLive!");
+  }
+}
