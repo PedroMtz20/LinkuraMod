6565cd735214194fca13329c542e6011354439ea
KCFindstr <792491320@qq.com>
2026-04-02 01:25:00 -0700

    feat: implement GlowingRoutinePower to burst hearts at the start of the turn

--- core/powers/GlowingRoutinePower.cs
+++ core/powers/GlowingRoutinePower.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
@@ -14,8 +15,9 @@ public class GlowingRoutinePower : LinkuraPower {
   public override PowerType Type => PowerType.Buff;
   public override PowerStackType StackType => PowerStackType.Counter;
 
-  public override async Task AfterPlayerTurnStart(PlayerChoiceContext ctx, Player player) {
-    if (player != Owner.Player) return;
-    await LinkuraCmd.BurstHearts(player, (int)Amount);
+  public override async Task BeforeSideTurnStart(PlayerChoiceContext choiceContext, CombatSide side, CombatState combatState) {
+    await base.BeforeSideTurnStart(choiceContext, side, combatState);
+    if (side != Owner.Side) return;
+    await LinkuraCmd.BurstHearts(Owner.Player, (int)Amount);
   }
 }
